13eda417698347aad1dcf08187ee007394f9fc32
DimasVeliz <trevor90094@gmail.com>
2021-04-24 12:35:44 +0200

    Conditionals fully explained

--- condicionales.c
+++ condicionales.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-     /*
+    /*
     6, REGLA del AND &&
 
     comer | irAlCine | SER FELIZ resultado AND
@@ -26,17 +26,16 @@ int main()
     0       1
     1       0 
     */
-    
 
-    int heComido =0;
-    int hayBuenClima =1;
+    int heComido = 0;
+    int hayBuenClima = 1;
     int serFeliz = heComido && hayBuenClima;
-    
+
     /*
     El if-else son mutuamente exclusivos
     el else, de alguna manera es una negacion del if
     */
-   
+
     if (serFeliz)
     {
         printf("Heheh ire al cine\n");
@@ -45,6 +44,47 @@ int main()
     {
         printf("Que pena\n");
     }
+
+    int mamaTrajoMandarinas = 0;
+    int mamaTrajoNaranjas = 0;
+    int noTengoHambre = mamaTrajoMandarinas || mamaTrajoNaranjas;
+
+    if (noTengoHambre)
+    {
+        printf("Menos mah\n");
+    }
+    else
+    {
+        printf("OStras madre, no me amas\n");
+        mamaTrajoMandarinas = 1;
+    }
+
+
+    /*
+    regla if-else if -else if..... else
+    TAMBIEN SON MUTUAMENTE EXCLUSIVOS
+    el else, de alguna manera es una negacion del if y todos los elif anteriores
+
+    */
+
+    int numero1 =7;
+    int numero2= 40;
     
+    if (numero1 > numero2)
+    {
+        printf("Numero1 es el mayor\n");
+    }
+    else if(numero1 < numero2 )
+    {
+        printf("Numero2 es el mayor\n");
+        numero2=numero1;
+    }
+    else
+    {
+        printf("Son iguales\n");
+    }
+
+
+    printf("Programa finalizado\n");
     return 0;
 }
